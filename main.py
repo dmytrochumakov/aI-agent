2216033c10d326cca6f840a7222bfcc1778b6322
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-06-10 15:06:38 +0300

    feat: messages

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 args = sys.argv
 
 if len(args) == 1:
     print("promt is not provided")
     sys.exit(1)
 
-promt = args[1]
+user_prompt = args[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=promt
+    model='gemini-2.0-flash-001', contents=messages
 )
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
