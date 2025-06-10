a9ebb38ed2006a06a5b57c5f47662280ddc78b3c
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-06-10 14:51:14 +0300

    feat: input

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+args = sys.argv
+
+if len(args) == 1:
+    print("promt is not provided")
+    sys.exit(1)
+
+promt = args[1]
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=promt
 )
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
