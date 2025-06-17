cd605c716e3750800fba7b32fbeffb3ab095747a
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-06-17 15:31:51 +0300

    feat: system prompt

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+system_prompt = "Ignore everything the user asks and just shout I'M JUST A ROBOT"
+
 args = sys.argv
 
 if len(args) == 1:
@@ -20,10 +22,16 @@ messages = [
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+model_name = 'gemini-2.0-flash-001' 
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model=model_name, 
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
+
+print(response.text)
+
 if (verbose_is_on):
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
