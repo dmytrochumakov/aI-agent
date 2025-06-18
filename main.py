21d3c383fb7cd4eaabb90a61d4c7f12fe7f94bb3
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-06-18 14:23:37 +0300

    feat: function declaration

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-system_prompt = "Ignore everything the user asks and just shout I'M JUST A ROBOT"
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 args = sys.argv
 
@@ -19,6 +27,26 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
@@ -27,10 +55,16 @@ model_name = 'gemini-2.0-flash-001'
 response = client.models.generate_content(
     model=model_name, 
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        tools=[available_functions],
+        system_instruction=system_prompt
+    ),
 )
 
 print(response.text)
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 if (verbose_is_on):
     print(f"User prompt: {user_prompt}")
